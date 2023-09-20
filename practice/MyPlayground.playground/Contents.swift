ba3e16c7cd47be944220a0eb9568f6c0d7de473a
youjiin0325 <youjiin0325@gmail.com>
2023-09-20 10:19:28 +0900

    learned how to use X code

---
+++ practice/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print("hello World", greeting)
+
+
+print("= Int32 =  \(Int32.min), Int 32 Max =>\(Int32.max)")
+print("= Int32=  \(Int64.min), Int 32 Max =>\(Int64.max)")
+print("= Int=  \(Int.min), Int Max =>\(Int.max)")
+
+//  부동소수점 타입
+let float = 1.1
+print("= float =>\(float)")
+print("= float =>\(float - 0.1)")
+
+//크냐 작냐 등의 조건식을, 비교해서 씀
+let bool = false
+print("= Bool => \(bool)")
+
+
+let char = "\u{0060}"
+print("= ? =? \(char)")
